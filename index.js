e93768f7589544bf88b60d2ed8217d47ebd84573
finanovita <finanovita479@gmail.com>
2025-11-04 14:22:33 +0700

    menjalankan server utama

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app =  express();
+const db = require('./models');
+const kandang = require('./models/kandang');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
